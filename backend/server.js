16537e0c832fea69af8921b4e5523d636b0d20d5
Md Mazdi Hasan <md.mazdi@gmail.com>
2020-10-31 03:20:51 +0600

    rename folder add nodemon dev

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,16 @@
 import express from 'express';
+import data from './data.js';
 const app = express();
+
+app.get('/api/products', (req, res) => {
+    res.send(data.products);
+})
 
 app.get('/', (req, res) =>{
     res.send('Server Is ready');
 });
 
-app.listen('5000', ()=>{
-    console.log('Server Is running On http://127.0.0.1:5000');
+const port = process.env.PORT || 5000
+app.listen(port, ()=>{
+    console.log(`Server Is running On http://127.0.0.1:${port}`);
 })
